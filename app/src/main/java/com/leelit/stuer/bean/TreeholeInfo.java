3aeb3334f5e8d7fd34b00cbcf685bf33b5307d54
leelit <475748491@qq.com>
2016-03-23 19:01:26 +0800

    prepare for module_treehole

---
+++ app/src/main/java/com/leelit/stuer/bean/TreeholeInfo.java
@@ -0,0 +1,25 @@
+package com.leelit.stuer.bean;
+
+/**
+ * Created by Leelit on 2016/3/23.
+ */
+public class TreeholeInfo {
+    String datetime;
+
+    String state;
+
+    String picAddress;
+
+    String uniquecode;
+
+    @Override
+    public String toString() {
+        return "TreeholeInfo{" +
+                "datetime='" + datetime + '\'' +
+                ", state='" + state + '\'' +
+                ", picAddress='" + picAddress + '\'' +
+                ", uniquecode='" + uniquecode + '\'' +
+                '}';
+    }
+
+}
